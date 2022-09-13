fadaace2314011e75c3ae5b15a4b05263d044f30
Evstro <Evstro.code@gmail.com>
2022-09-13 17:18:49 -0500

    Return on Investment
    Added within the stock_picker method an each_with_index method that will
    puts the ROI for each day of buying and selling.
    This happens regardless of whether the day to sell is
    after the day to buy.
    
    [Ticket: X]

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # stock-picker
+
+The Stock Picker program will take a price of a stock for each day and
+give the days to buy and sell to make the highest return on investment
--- stock-picker.rb
+++ stock-picker.rb
@@ -1,10 +1,21 @@
 # methods
-stock_picker(prices)
+def stock_picker(prices)
+  days = prices.length
+
+  prices.each_with_index do |price, day_to_buy|
+    day_to_sell = day_to_buy + 1
+    while day_to_sell < days do
+      return_on_investment = prices[day_to_buy] - prices[day_to_sell]
+      puts "Day to Buy: #{day_to_buy}, Day to Sell: #{day_to_sell}, ROI: #{return_on_investment}"
+      day_to_sell += 1
+    end
+  end
 
 end
 
 # variables
 prices = [17,3,6,9,15,8,6,1,10]
+
 
 # program
 stock_picker(prices)
